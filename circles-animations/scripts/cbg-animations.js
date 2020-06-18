9f9c20e7750ff5b9b0d17a72190532fe449ff174
Raul Martinez Insuasti <raul.alb.martinez@hotmail.com>
2020-06-18 12:32:57 -0400

    Circles Animations V1
    The code needs to be run under a server in order to work

---
+++ circles-animations/scripts/cbg-animations.js
@@ -0,0 +1,28 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('circle1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'scripts/circle1.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('circle2'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'scripts/circle2.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('circle3'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'scripts/circle3.json'
+})
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('circle4'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'scripts/circle4.json'
+})
